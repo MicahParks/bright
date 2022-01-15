03106cd3b25f533048f011546eb183c2993aea28
Micah Parks <git@micahparks.com>
2022-01-15 17:12:35 -0500

    Default to full brightness

--- main.go
+++ main.go
@@ -16,20 +16,27 @@ var (
 func main() {
 	logger := log.New(os.Stdout, "", 0)
 
+	var brightness *big.Float
+	var err error
 	const usage = "Argument must be a number between 0 and 100."
-	if len(os.Args) != 2 {
-		logger.Fatalln(usage)
-	}
 
-	brightness, _, err := big.ParseFloat(os.Args[1], 10, 0, big.ToPositiveInf)
-	if err != nil {
-		logger.Println(usage)
-		logger.Fatalf("Error: %s", err.Error())
+	if len(os.Args) == 1 {
+		brightness = big.NewFloat(1)
+	} else if len(os.Args) != 2 {
+		logger.Fatalln(usage)
+	} else {
+		brightness, _, err = big.ParseFloat(os.Args[1], 10, 0, big.ToPositiveInf)
+		if err != nil {
+			logger.Println(usage)
+			logger.Fatalf("Error: %s", err.Error())
+		}
+
+		if brightness.Cmp(min) == -1 || brightness.Cmp(max) == 1 {
+			logger.Fatalf("Must be between %s and %s.", min.String(), max.String())
+		}
+
+		brightness.Mul(brightness, big.NewFloat(.01))
 	}
-	if brightness.Cmp(min) == -1 || brightness.Cmp(max) == 1 {
-		logger.Fatalf("Must be between %s and %s.", min.String(), max.String())
-	}
-	brightness.Mul(brightness, big.NewFloat(.01))
 
 	out, err := exec.Command("xrandr", "--listactivemonitors").Output()
 	if err != nil {
